d979a44c53750ba814ef2744b5dfc4876e308d4b
VaniaSasikirana <sk080501@gmail.com>
2022-11-14 23:40:24 +0700

    kelarr uyeeeeeeeeeah

--- src/components/Project/MyProject.jsx
+++ src/components/Project/MyProject.jsx
@@ -4,15 +4,17 @@ const MyProject = () => {
         <br/>
         <br/>
         <br/>
-        <div className="container text-center projekKu">
-                <h1>BMI Calculator :</h1> <br/>
-                <iframe src="https://vaniaskirana-tpa2.netlify.app/" width={800} height={600}></iframe>
-                <br/><br/>
-                <h1>Fetch-API Movie TMDB :</h1> <br/>
-                <iframe src="https://vaniaskirana-tpa3.netlify.app/" width={800} height={600}></iframe>
-                <br/><br/><br/><br/>
+        <div className="row container text-center projekKu">
+            <div className="col">
                 <h1>Todo App ( React - Redux):</h1> <br/>
-                <iframe src="https://vaniaskirana-tpa5.vercel.app/" width={800} height={600}></iframe>
+                <iframe src="https://vaniaskirana-tpa5.vercel.app/" width={800} height={600}></iframe><hr/>
+
+                <h1>Fetch-API Movie TMDB :</h1> <br/>
+                <iframe src="https://vaniaskirana-tpa3.netlify.app/" width={800} height={600}></iframe><hr/>
+               
+                <h1>BMI Calculator :</h1> <br/><br/>
+                <iframe src="https://vaniaskirana-tpa2.netlify.app/" width={800} height={600}></iframe>
+                </div>
         </div>
         </>
     )
