1d5bd94a944eb69d6181b14010e22c3222cf3e33
VaniaSasikirana <sk080501@gmail.com>
2022-12-12 09:55:31 +0700

    update Skillbar

--- src/components/SkillPage/SkillBar.jsx
+++ src/components/SkillPage/SkillBar.jsx
@@ -1,4 +1,4 @@
-const SkillBar = ({percentageHtml, percentageCss, percentageJs, percentageJava, percentageC,percentageCC,percentageCCC, percentageGo}) => {
+const SkillBar = ({percentageHtml, percentageCss, percentageJs, percentageReact,percentageJava, percentageC,percentageCC,percentageCCC, percentageGo}) => {
     return(
         <>
             <br></br><br></br><br></br>
@@ -10,10 +10,19 @@ const SkillBar = ({percentageHtml, percentageCss, percentageJs, percentageJava, 
                 <div className="bar front advanced" data-skill="CSS">
                     <div className="percent">{percentageCss}</div>
                 </div>
+                <div className="bar front advanced" data-skill="Bootstrap">
+                    <div className="percent">{percentageCss}</div>
+                </div>
+                <div className="bar front advanced" data-skill="Git/Github">
+                    <div className="percent">{percentageCss}</div>
+                </div>
                 <div className="bar pre-advanced" data-skill="Javascript">
                     <div className="percent">{percentageJs}</div>
                 </div>
-                <div className="bar intermediate" data-skill="Java">
+                <div className="bar pre-advanced" data-skill="React JS">
+                    <div className="percent">{percentageReact}</div>
+                </div>
+                <div className="bar intermediate" data-skill="Redux">
                     <div className="percent">{percentageJava}</div>
                 </div>
                 <div className="bar back learning basic" data-skill="C">
--- src/pages/SkillPage.jsx
+++ src/pages/SkillPage.jsx
@@ -6,6 +6,7 @@ const SkillPage = () => {
     const [percentageHtml, setPercentageHtml] = useState("95%");
     const [percentageCss, setPercentageCss] = useState("90%");
     const [percentageJs, setPercentageJs] = useState("80%");
+    const [percentageReact, setPercentageReact] = useState("80%");
     const [percentageJava, setPercentageJava] = useState("60%");
     const [percentageC, setPercentageC] = useState("50%");
     const [percentageCC, setPercentageCC] = useState("50%");
@@ -13,7 +14,7 @@ const SkillPage = () => {
     const [percentageGo, setPercentageGo] = useState("50%");
     return (
         <>
-            <SkillBar percentageHtml={percentageHtml} percentageJs={percentageJs} percentageJava={percentageJava} percentageGo={percentageGo} percentageCss={percentageCss} percentageC={percentageC} percentageCC={percentageCC} percentageCCC={percentageCCC}/>
+            <SkillBar percentageHtml={percentageHtml} percentageReact={percentageReact} percentageJs={percentageJs} percentageJava={percentageJava} percentageGo={percentageGo} percentageCss={percentageCss} percentageC={percentageC} percentageCC={percentageCC} percentageCCC={percentageCCC}/>
             <SkillBadge/>
 
         </>
