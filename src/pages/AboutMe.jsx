5e1fb678bf568f7809eb65a8d9f8cc62157746ab
Vania Sasikirana <113840522+vaniaskirana@users.noreply.github.com>
2026-05-06 00:55:57 +0700

    Update AboutMe.jsx

--- src/pages/AboutMe.jsx
+++ src/pages/AboutMe.jsx
@@ -5,7 +5,7 @@ const AboutMe = () => {
     const [fullname, setFullName] = useState ("Vania Sasikirana");
     const [imgPas, setImgPas] = useState ("/img/pasme.png");
     const [desme1, setDesMe1] = useState ("Im study in , I am a student who are currently interested in the field technologies such as Web Development and Java Implementation.");
-    const [desme2, setDesMe2] = useState ("Soon i will be a Web Development. My future expertise is Create and Website Design, Graphic Design, etc..");
+    const [desme2, setDesMe2] = useState ("Soon i will be a Web Development. My future expertise is Create and Website Design, Graphic Design, etc....");
 
     return (
         <>
