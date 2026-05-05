a281a4843b2078b69d67c2cde82eb278abba0cf9
Vania Sasikirana <113840522+vaniaskirana@users.noreply.github.com>
2026-05-06 01:06:05 +0700

    Update AboutMe.jsx

--- src/pages/AboutMe.jsx
+++ src/pages/AboutMe.jsx
@@ -6,12 +6,10 @@ const AboutMe = () => {
     const [imgPas, setImgPas] = useState ("/img/pasme.png");
     const [desme1, setDesMe1] = useState ("Im study in , I am a student who are currently interested in the field technologies such as Web Development and Java Implementation.");
     const [desme2, setDesMe2] = useState ("Soon i will be a Web Development. My future expertise is Create and Website Design, Graphic Design, etc....");
-
     return (
         <>
         
-        <Biodata imgPas={imgPas}/>
-        <Experience/>
+    
 
         </>
     )
